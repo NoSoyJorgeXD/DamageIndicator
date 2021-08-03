1a71d42baa7212e09ae2e38a144b3fe67eae1ffd
SoyJorgeh <74513442+NoSoyJorgeXD@users.noreply.github.com>
2021-08-02 20:49:25 -0500

    Create SimpleEntity.php

---
+++ src/SoyJorgeh/DamageIndicator/SimpleEntity.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace SoyJorgeh\DamageIndicator;
+
+use pocketmine\entity\Human;
+
+class SimpleEntity extends Human {
+
+    protected $age = 0;
+
+    public static function init(): void {
+        parent::init();
+    }
+
+    public function entityBaseTick(int $tickDiff = 1): bool {
+
+        if ($this->closed){
+            return false;
+        }
+
+        $hasUpdate = parent::entityBaseTick($tickDiff);
+
+        if (!$this->isFlaggedForDespawn()){
+            $this->age += $tickDiff;
+            if ($this->age > 20);
+                $this->flagForDespawn();
+        }
+    }
+
+    return $hasUpdate;
+
+
+}
